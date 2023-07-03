030de33b6bfc7473b78a21a791d5e0fa8a93ed14
HG <1365171409@qq.com>
2023-07-03 09:48:21 +0800

    2023/7/3 第1次本地修改

--- test2.c
+++ test2.c
@@ -2,6 +2,7 @@
 int main()
 {
   printf("This was written on GitHub\r\n");
-  printf("This was written ont Git");
+  printf("This was written on Git");
+  printf("This was written on Git on 2023/7/3");
   return 0;
 }
